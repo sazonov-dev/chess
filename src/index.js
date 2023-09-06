67df082c87c778013acd1eb94933437b4db6e1a5
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-09-06 15:51:17 +0300

    added team section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './scss/styles.scss';
+
+const updateActiveDot = () => {
+    const scrollContainer = document.querySelector('.team-top__players');
+    const dots = document.querySelectorAll('.dot');
+    const cardWidth = scrollContainer.querySelector('.team-top__players-card').offsetWidth;
+    const scrollLeft = scrollContainer.scrollLeft;
+    const activeIndex = Math.floor(scrollLeft / cardWidth);
+
+    dots.forEach(dot => dot.classList.remove('active-dot'));
+
+    if (dots[activeIndex]) {
+        dots[activeIndex].classList.add('active-dot');
+    }
+}
+
+const appendDots = (count, section) => {
+    for (let i = 0; i < count; i++) {
+        const dot = document.createElement('div')
+        if (i === 0) {
+            dot.classList.add('dot', 'active-dot');
+        } else {
+            dot.classList.add('dot');
+        }
+        section.append(dot);
+    }
+}
+
+if (document.querySelector('#general')) {
+    const scrollContainer = document.querySelector('.team-top__players');
+    const scrollContainerLength = Array.from(scrollContainer.querySelectorAll('.team-top__players-card')).length;
+    const dotContainer = document.querySelector('.dots-container');
+
+    appendDots(scrollContainerLength, dotContainer);
+    scrollContainer.addEventListener('scroll', updateActiveDot);
+}
